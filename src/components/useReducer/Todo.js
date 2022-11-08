bd32c4ea9f417fc3ce65b9449cd08d05ea75a86b
Benjamin [TGP] <benjamin@thegoodplace.nl>
2022-11-08 16:56:11 +0100

    useReducer added

---
+++ src/components/useReducer/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ACTIONS } from './UseReducer.js'
+
+export default function Todo({todo, dispatch}) {
+  return (
+    <div>
+        <span style={{ color: todo.complete ? 'red' : 'green' }}>{todo.name}</span>
+        <button onClick={() => dispatch({type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id }})}>Toggle</button>
+        <button onClick={() => dispatch({type: ACTIONS.DELETE_TODO, payload: { id: todo.id }})}>Delete</button>
+    </div>
+  )
+}
